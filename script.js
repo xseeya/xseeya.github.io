9394cd1b40ea5592ba60f4d813d876f229bf533f
xseeya <115592165+xseeya@users.noreply.github.com>
2025-08-29 21:17:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
